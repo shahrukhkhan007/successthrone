4d0c1b590761a5f566e61d90f34ba98e323b2ebf
shahrukh_khan <shahrukh17khan13@gmail.com>
2022-03-04 20:48:55 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(redirectUnmatched);
+app.all("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname , "/build/index.html"));
+});
+
+
+
+function redirectUnmatched(req, res) {
+  res.redirect("/");
+}
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("Server listening on port 8000!");
+});
